c4c31f2d7716f3780e3e8314f5b6b582866cb8d2
软通动力-何永强 <yqheq@isoftstone.com>
2019-03-29 18:46:04 +0800

    d

--- node-server.js
+++ node-server.js
@@ -5,7 +5,7 @@ let fs = require('fs')
 let VueServerRenderer = require('vue-server-renderer')
 let serverBundle = fs.readFileSync('./dist/server.bundle.js', 'utf8')
 let ssrTemplate = fs.readFileSync('./dist/index.ssr.html', 'utf8')
-let render = VueServerRenderer.createBundleRenderer(serverBundle, {
+let render = VueServerRenderer.createRenderer(serverBundle, {
     template: ssrTemplate
 })
 app.get('/', (req, res) => {
